e48a212881102bcc91371449619e53a6889a74d1
Yogendra Kumar <yogendraidea@gmail.com>
2026-08-09 23:36:44 +0530

    New6

--- Child-Module/RG/Main.tf
+++ Child-Module/RG/Main.tf
@@ -1,5 +1,6 @@
 resource "azurerm_resource_group" "rg" {
-for_each = var.rgs
+for_each = var.rgs 
   name     = each.value.rg-name
   location = each.value.rg-location
 }
+
--- Child-Module/RG/Variable.tf
+++ Child-Module/RG/Variable.tf
@@ -1,3 +1,1 @@
-variable "rgs" {
-    type = string
-}
+variable "rgs" {}
